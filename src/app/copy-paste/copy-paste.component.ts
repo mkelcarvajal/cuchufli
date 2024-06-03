c937eacef2b34923cb26d94d0f39d9f695588880
Maikel Carvajal Ortiz <maikel.carvajal@alumnos.ucn.cl>
2024-06-02 22:05:30 -0400

    feature: mejoras de detectado de datos

--- src/app/copy-paste/copy-paste.component.ts
+++ src/app/copy-paste/copy-paste.component.ts
@@ -37,6 +37,7 @@ export class CopyPasteComponent {
     }
 
     if (data.bank?.toLowerCase().includes('santander')) { // Check for Banco Santander
+      console.log(data.accountNumber);
       data.accountNumber = data.accountNumber.replace('-', '').replace('.', ''); // Remove all separators
     } else {
       data.accountNumber = data.accountNumber.replace('-', '').replace(' ', '').replace('.', ''); // Remove all separators except hyphen
@@ -70,38 +71,113 @@ export class CopyPasteComponent {
     return data;
   }
 
-  getBankData(fileData: string[]) {
-    const indexes: number[] = [];
-    let data: any = {}; // Create copy to avoid mutating original data
-    if (fileData[0].toLowerCase().includes('santander')) {
-      indexes.push(0, 8, 6, 4, 10, 12);
-    } else if (fileData[0].toLowerCase().includes('falabella')) {
-      indexes.push(0, 2, 4, 6, 8, 10);
-    } else if (fileData[0].toLowerCase().includes('estado')) {
-      indexes.push(0, 8, 6, 4, 10, 12);
-    } else if (fileData[0].toLowerCase().includes('bci')) {
-      indexes.push(0, 8, 4, 6, 10, 2);
-    } else if (fileData[0].toLowerCase().includes('chile')) {
-      indexes.push(0, 8, 6, 4, 10, 12);
+
+  getBankData(fileData: any, datos: any = {}) {
+    let indexes: any = [];
+
+    try {
+      if (fileData[0].toLowerCase().includes('santander')) {
+        indexes = [0, 8, 6, 4, 10, 12];
+      } else if (fileData[0].toLowerCase().includes('falabella')) {
+        if (!fileData[7].includes('Correo electronico')) {
+          indexes = [0, 2, 4, 6, 8];
+        } else {
+          indexes = [0, 2, 4, 6, 8, 10];
+        }
+      } else if (fileData[0].toLowerCase().includes('estado')) {
+        indexes = [0, 8, 6, 4, 10, 12];
+      } else if (fileData[0].toLowerCase().includes('bci')) {
+        indexes = [0, 8, 4, 6, 10, 2];
+      } else if (fileData[0].toLowerCase().includes('chile')) {
+        indexes = [0, 8, 6, 4, 10, 12];
+      } else if (fileData[0].toLowerCase().includes('banco específico')) {
+        indexes = [4, 8, 6, 2];
+      }
+
+      if (indexes.length) {
+        const values = indexes;
+        datos.bank = fileData[values[0]];
+        datos = this.validateBank(datos);
+        datos.rut = fileData[values[1]];
+        datos = this.validateRut(datos);
+        datos.accountNumber = fileData[values[2]];
+        datos = this.validateAccountNumber(datos);
+
+        //     data['bank'] = fileData[indexes[0]];
+        //     data = this.validateBank(data); // Assuming validateBank is a separate function
+        //     data['rut'] = fileData[indexes[1]];
+        //     data['rut'] = data['rut'].replaceAll(".", "")
+        //     data = this.validateRut(data); // Assuming validateRut is a separate function
+        //     data['accountNumber'] = fileData[indexes[2]];
+        //     data = this.validateAccountNumber(data); // Assuming validateAccountNumber is a separate function
+        //     data['accountType'] = fileData[indexes[3]];
+        //     data = this.validateAccountType(data); // Assuming validateAccountType is a separate function
+        //     data['email'] = fileData[indexes[4]];
+        //     data['name'] = fileData[indexes[5]];
+        //     data['accountNumber'] = data['accountNumber'].replaceAll("-", "")
+
+        if (fileData[0].toLowerCase().includes('banco específico')) {
+          datos.accountType = 'Cuenta Corriente';
+          datos.email = '';
+          datos.name = fileData[values[3]];
+        } else {
+          datos.accountType = fileData[values[3]];
+          datos = this.validateAccountType(datos);
+
+          if (!fileData[7].includes('Correo electronico') && fileData[0].toLowerCase().includes('falabella')) {
+            datos.email = '';
+            datos.name = fileData[values[4]];
+          } else {
+            datos.email = fileData[values[4]];
+            datos.name = fileData[values[5]];
+          }
+        }
+
+        if (!datos.accountType.toLowerCase().includes('vista') || datos.accountType.toLowerCase().includes('corriente')) {
+          datos.accountType = 'Cuenta Corriente';
+        }
+      }
+      datos.accountNumber = Number(datos.accountNumber)
+    } catch (error) {
+      console.log(error);
+      console.error('Error en el archivo');
     }
 
-    if (indexes.length) {
-      data['bank'] = fileData[indexes[0]];
-      data = this.validateBank(data); // Assuming validateBank is a separate function
-      data['rut'] = fileData[indexes[1]];
-      data['rut'] = data['rut'].replaceAll(".", "")
-      data = this.validateRut(data); // Assuming validateRut is a separate function
-      data['accountNumber'] = fileData[indexes[2]];
-      data = this.validateAccountNumber(data); // Assuming validateAccountNumber is a separate function
-      data['accountType'] = fileData[indexes[3]];
-      data = this.validateAccountType(data); // Assuming validateAccountType is a separate function
-      data['email'] = fileData[indexes[4]];
-      data['name'] = fileData[indexes[5]];
-      data['accountNumber'] = data['accountNumber'].replaceAll("-", "")
-    }
-
-    return data;
+    return datos;
   }
+
+  // getBankData(fileData: string[]) {
+  //   const indexes: number[] = [];
+  //   let data: any = {}; // Create copy to avoid mutating original data
+  //   if (fileData[0].toLowerCase().includes('santander')) {
+  //     indexes.push(0, 8, 6, 4, 10, 12);
+  //   } else if (fileData[0].toLowerCase().includes('falabella')) {
+  //     indexes.push(0, 2, 4, 6, 8, 10);
+  //   } else if (fileData[0].toLowerCase().includes('estado')) {
+  //     indexes.push(0, 8, 6, 4, 10, 12);
+  //   } else if (fileData[0].toLowerCase().includes('bci')) {
+  //     indexes.push(0, 8, 4, 6, 10, 2);
+  //   } else if (fileData[0].toLowerCase().includes('chile')) {
+  //     indexes.push(0, 8, 6, 4, 10, 12);
+  //   }
+
+  //   if (indexes.length) {
+  //     data['bank'] = fileData[indexes[0]];
+  //     data = this.validateBank(data); // Assuming validateBank is a separate function
+  //     data['rut'] = fileData[indexes[1]];
+  //     data['rut'] = data['rut'].replaceAll(".", "")
+  //     data = this.validateRut(data); // Assuming validateRut is a separate function
+  //     data['accountNumber'] = fileData[indexes[2]];
+  //     data = this.validateAccountNumber(data); // Assuming validateAccountNumber is a separate function
+  //     data['accountType'] = fileData[indexes[3]];
+  //     data = this.validateAccountType(data); // Assuming validateAccountType is a separate function
+  //     data['email'] = fileData[indexes[4]];
+  //     data['name'] = fileData[indexes[5]];
+  //     data['accountNumber'] = data['accountNumber'].replaceAll("-", "")
+  //   }
+
+  //   return data;
+  // }
 
   splitStringByLineBreaks(text: string): string[] {
     // Replace multiple consecutive line breaks with a single one
